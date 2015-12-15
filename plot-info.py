d93d7eb7eed81c0a8b875da27c12ae9c1786a0a8
Michael A. Perlin <mika.perlin@gmail.com>
2015-12-15 18:07:52 +0100

    template script for interpreting iswap_fidelity results

---
+++ plot-info.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from pylab import *
+
+w, A, A_perp, dw_min, f_DD, operation_time, fidelity = loadtxt('iswap_info.txt', unpack=True)
+
+mxi = fidelity == max(fidelity)
+
+print(fidelity[mxi])
+print(operation_time[mxi])
+
+
+figure()
+title('fidelity histogram (scale$=100$)')
+hist(fidelity[fidelity > 0.9])
+xlabel('fidelity')
+
+show()
