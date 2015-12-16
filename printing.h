fa686944c5d111ed8aabded1f2110548ebb75450
Michael A. Perlin <mika.perlin@gmail.com>
2015-12-16 12:12:02 +0100

    added come consts and uints

--- printing.h
+++ printing.h
@@ -16,9 +16,9 @@ inline MatrixXcd clean(const MatrixXcd& M){
 
 // returns element p of a basis for operators acting on a system with N qubits
 string U_basis_element_text(const uint p, const uint N){
-  char spins[4] = {'I','X','Y','Z'};
+  const char spins[4] = {'I','X','Y','Z'};
   stringstream stream;
-  for(int n = 0; n < N; n++){
+  for(uint n = 0; n < N; n++){
     stream << spins[int_bit(p,2*n)+2*int_bit(p,2*n+1)];
   }
   return stream.str();
@@ -26,8 +26,8 @@ string U_basis_element_text(const uint p, const uint N){
 
 // print operator in human-readable form
 void U_print(const MatrixXcd& U, const bool fast = true){
-  int N = log2(U.rows());
-  MatrixXcd hs = clean(U_decompose(U,fast));
+  const int N = log2(U.rows());
+  const MatrixXcd hs = clean(U_decompose(U,fast));
   for(int p = 0; p < pow(4,N); p++){
     if(abs(hs(p)) != 0){
       cout << U_basis_element_text(p,N) << ": " << hs(p) << endl;
@@ -37,12 +37,12 @@ void U_print(const MatrixXcd& U, const bool fast = true){
 
 // print state vector in human readable form
 void state_print(const MatrixXcd& psi){
-  int N = psi.size();
-  int qbits = log2(N);
-  for(int n = 0; n < N; n++){
+  const uint N = psi.size();
+  const uint qbits = log2(N);
+  for(uint n = 0; n < N; n++){
     if(abs(psi(n)) != 0){
       cout << "|";
-      for(int q = 0; q < qbits; q++){
+      for(uint q = 0; q < qbits; q++){
         cout << (qbit_state(q,qbits,n)?"d":"u");
       }
       cout << "> " << psi(n) << endl;
@@ -52,16 +52,16 @@ void state_print(const MatrixXcd& psi){
 
 // print matrix in human readable form
 void matrix_print(const MatrixXcd& M){
-  int qbits = log2(M.rows());
-  for(int m = 0; m < M.rows(); m++){
-    for(int n = 0; n < M.cols(); n++){
+  const int qbits = log2(M.rows());
+  for(uint m = 0; m < M.rows(); m++){
+    for(uint n = 0; n < M.cols(); n++){
       if(abs(M(m,n)) != 0){
         cout << "|";
-        for(int q = 0; q < qbits; q++){
+        for(uint q = 0; q < qbits; q++){
           cout << (qbit_state(m,qbits,q)?"d":"u");
         }
         cout << "><";
-        for(int q = 0; q < qbits; q++){
+        for(uint q = 0; q < qbits; q++){
           cout << (qbit_state(n,qbits,q)?"d":"u");
         }
         cout << "| " << M(m,n) << endl;
