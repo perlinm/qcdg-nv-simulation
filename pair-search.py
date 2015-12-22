fcaf69577793966739e9911441d30305c484d005
Michael A. Perlin <mika.perlin@gmail.com>
2015-12-22 16:03:35 +0100

    minor changes to pair-search.py

--- pair-search.py
+++ pair-search.py
@@ -2,14 +2,17 @@
 
 import os, sys, subprocess, random
 
-if len(sys.argv) != 3:
-    print("usage: {} samples hyperfine_cutoff (in kHz)".format(sys.argv[0]))
+if len(sys.argv) not in [2,3,4]:
+    print("usage: {} hyperfine_cutoff_in_kHz [samples] [c13_abundance]".format(sys.argv[0]))
     exit(1)
 
-samples = sys.argv[1]
-hyperfine_cutoff = sys.argv[2]
+hyperfine_cutoff = sys.argv[1]
+try:
+    samples = int(sys.argv[2])
+except:
+    samples = 100
 
-random.seed(samples+hyperfine_cutoff)
+random.seed(str(samples)+hyperfine_cutoff)
 def rnd():
     return int(random.random()*1e6)
 
@@ -17,11 +20,13 @@ with open(os.devnull, 'w') as null:
     subprocess.call(['fac'],stdout=null)
 
     found = 0
-    for s in range(int(samples)):
+    for s in range(samples):
         commands = ["./simulate",
                     "--pair_search",
                     "--hyperfine_cutoff",hyperfine_cutoff,
                     "--seed",str(rnd())]
+        if len(sys.argv) == 4:
+            commands += ["--c13_abundance", sys.argv[3]]
         found += subprocess.call(commands,stdout=null)
 
-print(sys.argv[2],found,samples)
+print(found/samples)
