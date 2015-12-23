cc894fea8e357f4bfdb3b34cf249f9f0ca817aff
Michael A. Perlin <mika.perlin@gmail.com>
2015-12-23 15:42:02 +0100

    make sample number a required argument in pair-test.py

--- pair-test.py
+++ pair-test.py
@@ -6,15 +6,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if len(sys.argv) not in [3,4]:
-    print("usage: {} cutoff_start cutoff_end [samples]".format(sys.argv[0]))
+    print("usage: {} cutoff_start cutoff_end samples".format(sys.argv[0]))
     exit(1)
 
 start = int(sys.argv[1])
 end = int(sys.argv[2])
-try:
-    samples = sys.argv[3]
-except:
-    samples = "100"
+samples = int(sys.argv[3])
 
 if not start < end:
     print("cutoff start must be less than end")
@@ -31,7 +28,7 @@ if not os.path.exists(fname):
     for i in range(len(cutoffs)):
         print("starting cutoff: {} kHz".format(cutoffs[i]))
         predicted[i] = sp.check_output(["./pair-probability.py",str(cutoffs[i])])
-        actual[i] = sp.check_output(["./pair-search.py",str(cutoffs[i]),samples])
+        actual[i] = sp.check_output(["./pair-search.py",str(cutoffs[i]),str(samples)])
 
     with open(fname,'w') as f:
         f.write("# samples: {}\n".format(samples))
