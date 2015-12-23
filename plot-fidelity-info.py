6afd716a25611c41cb67b6e4876a5f98a0a98384
Michael A. Perlin <mika.perlin@gmail.com>
2015-12-23 14:52:56 +0100

    cleaned up plot-fidelity-info.py a bit

--- plot-fidelity-info.py
+++ plot-fidelity-info.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-from pylab import *
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
 if len(sys.argv) != 2:
     print("useage: %s file" % sys.argv[0])
@@ -11,7 +14,7 @@ if "fidelities" not in fname:
     print("invalid input file")
     exit(1)
 
-index, w, A, A_perp, dw_min, f_DD, operation_time, fidelity = loadtxt(fname, unpack=True)
+index, w, A, A_perp, dw_min, f_DD, operation_time, fidelity = np.loadtxt(fname, unpack=True)
 
 mxi = fidelity == max(fidelity)
 print("max fidelity:",max(fidelity))
@@ -20,9 +23,7 @@ print("operation time(s):",operation_time[mxi])
 cutoff = 0.95
 print("fidelities above {}: {}".format(cutoff,fidelity[fidelity > cutoff]))
 
-f2 = multiply(fidelity,fidelity)
+f2 = np.multiply(fidelity,fidelity)
 
-figure()
-hist(f2[f2 > 0.5])
-
-show()
+plt.hist(f2[f2 > 0.5])
+plt.show()
