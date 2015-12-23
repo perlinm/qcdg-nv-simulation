fixed an off-by-one error in pair-probability.py

pair-probability.py
@@ -31,7 +31,7 @@ gc_SI = 67.28284e6 # gyromagnetic ratio of C-13 (Hz/tesla)
 alpha = 1/137.035999074 # fine structure constant
 qe = np.sqrt(4*np.pi*alpha) # unit electric charge
 me = 510998.928/hbar_SI # mass of electron (Hz)
-ge = qe/(2*me) * 2.0023193043617 # gyromagnetic ratio of electron
+ge = -qe/(2*me) * 2.0023193043617 # gyromagnetic ratio of electron
 gc = gc_SI * ge/ge_SI # gyromagnetic ratio of C-13
 
 sec = 1 # one second in natural units (s)
@@ -54,20 +54,20 @@ def A(b,l,m,n):
     return norm( ge*gc/(4*np.pi*(norm(r)*a0)**3) * (zhat - 3*np.dot(hat(r),zhat)*hat(r)) )
 
 # maximum allowable value of l^2, m^2, or n^2
-M = (2*ge*gc / (np.pi * a0**3 * hyperfine_cutoff))**(2/3)
+M = (2*abs(ge*gc) / (np.pi * a0**3 * hyperfine_cutoff))**(2/3)
 
 # list of all equivalence classes of integers (b,l,m,n)
 #   i.e. sets {(b,l,m,n)} within which both l^2+m^2+n^2 and |1.5*b+l+m+n| are constant
 equivalence_classes = []
 
 # loop over each integer N < M
-for N in range(1, int(M)+1):
+for N in range(1, int(np.ceil(M))+1):
 
     # sets of integers {l,m,n} satisfying l^2+m^2+n^2 == N and l^2+m^2+n^2+(l+m+n)^2 < M
     square_solution_sets = set([ (sign_l*l,sign_m*m,sign_n*n)
-                                 for l in range(0, int(np.sqrt(N))+1)
-                                 for m in range(l, int(np.sqrt(N))+1)
-                                 for n in range(m, int(np.sqrt(N))+1)
+                                 for l in range(0, int(np.ceil(np.sqrt(N)))+1)
+                                 for m in range(l, int(np.ceil(np.sqrt(N)))+1)
+                                 for n in range(m, int(np.ceil(np.sqrt(N)))+1)
                                  if l*l+m*m+n*n == N
                                  for sign_l in [1,-1]
                                  for sign_m in [1,-1]
@@ -82,10 +82,11 @@ for N in range(1, int(M)+1):
 
     # determine all equivalence classes of integers (b,l,m,n) for this value of N
     for b in [0,1]:
-        blmn_sums = list(set([ abs(1.5*b+l+m+n) for (l,m,n) in square_solutions ]))
+        blmn_sums = list(set([ abs(1.5*b+l+m+n) for (l,m,n) in square_solutions
+                               if A(b,l,m,n) > hyperfine_cutoff ]))
         for s in blmn_sums:
             equivalence_class = [ (b,l,m,n) for (l,m,n) in square_solutions
-                                  if abs(1.5*b+l+m+n) == s and A(b,l,m,n) > hyperfine_cutoff ]
+                                  if abs(1.5*b+l+m+n) == s ]
             if len(equivalence_class) > 1:
                 equivalence_classes.append(equivalence_class)
 