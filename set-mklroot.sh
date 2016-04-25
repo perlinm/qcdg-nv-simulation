569c8579fdadf58a4daae5ebfeb1817f3b81701c
Michael A. Perlin <mika.perlin@gmail.com>
2016-04-25 15:52:03 +0200

    fixed bug in set-mklroot which incorrectly identified host OS release

--- set-mklroot.sh
+++ set-mklroot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ -n "cat /etc/*-release | grep 'Arch Linux'" ]; then
+if [ -n "$(cat /etc/*-release | grep 'Arch Linux')" ]; then
   MKLROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mkl
 fi
 
