5af192de2f2d2916f5214c4267d60e67d4d10eaf
Michael A. Perlin <mika.perlin@gmail.com>
2016-04-25 17:06:15 +0200

    expand paths of local eigen libraries

--- set-eigen-dirs.py
+++ set-eigen-dirs.py
@@ -3,7 +3,8 @@ import os
 
 eigen_dirs = "-I /usr/include/eigen3/"
 if not os.path.isdir(eigen_dirs.split()[-1]):
-    eigen_dirs = "-I ~/.local/include/ -I ~/.local/include/eigen3/"
+    eigen_libs = ["~/.local/include/","~/.local/include/eigen3/"]
+    eigen_dirs = " ".join(["-I " + os.path.expanduser(lib) for lib in eigen_libs])
 
 with open(".eigen-dirs","w") as f:
     f.write(eigen_dirs+"\n")
