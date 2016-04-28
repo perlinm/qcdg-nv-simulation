b11fe5d7286b86d44831def0fa69a21d7b8876f0
Michael A. Perlin <mika.perlin@gmail.com>
2016-04-28 17:27:43 +0200

    made resource specifications compatible with JUSTUS cluster format

--- run-sweep.py
+++ run-sweep.py
@@ -3,12 +3,12 @@ import sys, os, random, string, subprocess
 
 if len(sys.argv) != 8:
     print("usage: " + sys.argv[0] + " sim_type static_Bz c13_abundance" + \
-          " max_cluster_size log10_samples task_num walltime")
+          " max_cluster_size log10_samples nodes walltime")
     exit(1)
 
 script = os.path.abspath("fidelity-sweep.py")
 sim_args = sys.argv[1:6]
-task_num = sys.argv[-2]
+nodes = sys.argv[-2]
 walltime = sys.argv[-1]
 assert int(task_num) > 1
 
@@ -17,10 +17,15 @@ basename = "-".join(sim_args)
 out_file = job_dir+"/"+basename+".o"
 err_file = job_dir+"/"+basename+".e"
 
+tasks_per_node = 16
+task_num = str(int(nodes)*tasks_per_node)
+resources = "nodes={}:ppn={},walltime={},pmem=10mb".format(nodes,tasks_per_node,walltime)
+
 temp_file = "".join(random.sample(string.lowercase,10))
 with open(temp_file,"w") as f:
     f.write("python {} {} {}\n".format(script," ".join(sim_args),task_num))
-subprocess.call(["msub", "-m", "e", "-N", basename, "-o", out_file, "-e", err_file,
-                 "-l", "walltime={},pmem=100mb,ttc={}".format(task_num,walltime),
-                 temp_file])
+
+subprocess.call(["msub", "-m", "e", "-N", basename,
+                 "-o", out_file, "-e", err_file,
+                 "-l", resources, temp_file])
 os.remove(temp_file)
