194eb27851134e025026b192da516acf3597bfb0
Michael A. Perlin <mika.perlin@gmail.com>
2016-05-18 17:20:14 +0200

    minor cleaning up in set-eigen-dirs.py

--- set-eigen-dirs.py
+++ set-eigen-dirs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os
 
-eigen_dirs = "-I /usr/include/eigen3/"
-if not os.path.isdir(eigen_dirs.split()[-1]):
+eigen_libs = ["/usr/include/eigen3/"]
+if not all([ os.path.isdir(lib) for lib in eigen_libs ]):
     eigen_libs = ["~/.local/include/","~/.local/include/eigen3/"]
-    eigen_dirs = " ".join(["-I " + os.path.expanduser(lib) for lib in eigen_libs])
 
+eigen_dirs = " ".join(["-I " + os.path.expanduser(lib) for lib in eigen_libs])
 with open(".eigen-dirs","w") as f:
     f.write(eigen_dirs+"\n")
