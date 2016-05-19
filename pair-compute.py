97a8dea0647c73f8200dfe58618e22c6f4c6b11a
Michael A. Perlin <mika.perlin@gmail.com>
2016-05-19 12:34:25 +0200

    changed some naming conventions and removed some obsolete lines of code in pair-compute.py

--- pair-compute.py
+++ pair-compute.py
@@ -39,48 +39,44 @@ a3 = np.array([1,1,0])
 norm = np.linalg.norm
 def hat(v): return v/norm(v)
 def A(b,l,m,n):
-    r = b*ao+l*a1+m*a2+n*a3
+    r = b*ao + l*a1 + m*a2 + n*a3
     return norm( ge*gC13/(4*np.pi*(norm(r)*a0/2)**3) *
                  (zhat-3*np.dot(hat(r),zhat)*hat(r)) )
 
+def z_projection_sum(iv): return abs(3*iv[0] + 4*(iv[1]+iv[2]+iv[3]))
+def xy_projection_sum(iv): return 3*(iv[1]**2+iv[2]**2+iv[3]**2) - (iv[1]+iv[2]+iv[3])**2
+
 # maximum allowable magnitude of l, m, or n
-M = int((2*abs(ge*gC13) / (np.pi * a0**3 * hyperfine_cutoff))**(1./3)+1./2)
+max_lmn = int((2*abs(ge*gC13) / (np.pi * a0**3 * hyperfine_cutoff))**(1./3)+1./2)
 
-# list of all equivalence classes of integers (b,l,m,n)
-equivalence_classes = []
+# list of all larmor sets
+larmor_sets = []
 
-# loop over each quarter-integer N <= 3*M (N_4 = 4*N)
-for N_times_4 in range(1,12*M+1):
+# loop over each integer z_sum up to the maximum possible value given the hyperfine cutoff
+# start at 1 to exclude solutions in the x-y plane
+for z_sum in range(1,12*max_lmn+1):
 
-    # vector of integers (b,l,m,n) satisfying abs(l+m+n) == N
-    sum_solutions = [ (b,l,m,n)
-                      for b in [0,1]
-                      for l in range(-M,M+1)
-                      for m in range(-M,M+1)
-                      for n in range(-M,M+1)
-                      if abs(3*b + 4*(l+m+n)) == N_times_4
-                      if A(b,l,m,n) > hyperfine_cutoff ]
+    # vector of integers (b,l,m,n) with this z_sum
+    z_sum_solutions = set([ (b,l,m,n)
+                            for b in [0,1]
+                            for l in range(-max_lmn,max_lmn+1)
+                            for m in range(-max_lmn,max_lmn+1)
+                            for n in range(-max_lmn,max_lmn+1)
+                            if z_projection_sum((b,l,m,n)) == z_sum
+                            if A(b,l,m,n) > hyperfine_cutoff ])
 
-    # if sum_solutions is empty, skip ahead to the next value of N
-    if(len(sum_solutions) == 0): continue
+    # determine all xy_sums for these solutions of z_sum
+    xy_sums = set([ xy_projection_sum(z_solution) for z_solution in z_sum_solutions ])
 
-    # determine all equivalence classes of integers (b,l,m,n) for this value of N
-    lmn_square_sums = set([ 3*(l*l+m*m+n*n)-(l+m+n)*(l+m+n)
-                            for (b,l,m,n) in sum_solutions ])
-
-    for ss in lmn_square_sums:
-
-        # if this larmor set lies on the z axis, don't count it
-        if ss == 0: continue
-
-        equivalence_class = [ (b,l,m,n) for (b,l,m,n) in sum_solutions
-                              if 3*(l*l+m*m+n*n)-(l+m+n)*(l+m+n) == ss ]
-        equivalence_classes.append(equivalence_class)
+    # collect solutions of z_sum into larmor sets if they have the same xy_sum
+    for xy_sum in xy_sums:
+        larmor_set = [ z_solution for z_solution in z_sum_solutions
+                       if xy_projection_sum(z_solution) == xy_sum ]
+        larmor_sets.append(larmor_set)
 
 # return a the sizes of the nontrivial parallel subsets in a given equivalence class
-def parallel_subset_sizes(equivalence_class):
-    r_xy_vecs = [ np.array([-2*l+m+n,-2*m+n+l,-2*n+l+m])
-                  for _,l,m,n in equivalence_class ]
+def parallel_subset_sizes(larmor_set):
+    r_xy_vecs = [ np.array([-2*l+m+n,-2*m+n+l,-2*n+l+m]) for _,l,m,n in larmor_set ]
 
     parallel_sets = []
     added_to_set = []
@@ -100,10 +96,8 @@ def parallel_subset_sizes(equivalence_class):
     parallel_set_sizes = [ len(parallel_set) for parallel_set in parallel_sets ]
     return [ size for size in parallel_set_sizes if size > 1 ]
 
-equivalence_class_info = [ (len(equivalence_class),
-                            parallel_subset_sizes(equivalence_class))
-                           for equivalence_class in equivalence_classes ]
-equivalence_class_info.sort()
+larmor_set_info = [ (len(larmor_set), parallel_subset_sizes(larmor_set))
+                    for larmor_set in larmor_sets ]
 
 # return product of elements in a list
 def product(list):
@@ -116,8 +110,8 @@ def nCk(n,k): return product([ n+1-i for i in range(1,k+1)]) / product(range(1,k
 
 # probability of having at least one larmor set with exactly two addressable nuclei
 probability = 1 - product([ 1 - c13_abundance**2 * (1-c13_abundance)**(R-2) *
-                            ( nCk(R,2) - sum([ nCk(Sp,2) for Sp in pss ]) )
-                            for eci in equivalence_class_info
+                            ( nCk(R,2) - sum([ nCk(s,2) for s in pss ]) )
+                            for eci in larmor_set_info
                             for R in [eci[0]]
                             for pss in [eci[1]] ])
 print(probability)
