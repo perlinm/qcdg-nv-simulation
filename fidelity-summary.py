330b1a0f5ee26246406750df35dacd1ae623f4f2
Michael A. Perlin <mika.perlin@gmail.com>
2016-06-06 12:08:27 +0200

    stop fidelity summary at some cutoff (0.5 by default)

--- fidelity-summary.py
+++ fidelity-summary.py
@@ -2,11 +2,16 @@
 import sys, os, re
 from basename import basename
 
-if len(sys.argv) != 2:
-    print("usage: {} fidelity_data_file".format(sys.argv[0]))
+if len(sys.argv) not in [2,3]:
+    print("usage: {} fidelity_data_file [fidelity_cutoff]".format(sys.argv[0]))
     exit(1)
 
 fname = sys.argv[1]
+if len(sys.argv) == 3:
+    fidelity_cutoff = float(sys.argv[2])
+else:
+    fidelity_cutoff = 0.5
+
 if not os.path.isfile(fname):
     print("invalid file: {}".format(fname))
     exit(1)
@@ -38,12 +43,18 @@ with open(fname,'r') as f:
                 exit(1)
             results += [(float(line[-2]),float(line[-1]),seed,targets)]
 
-results.sort(key = lambda x: -x[0])
+if len(results) == 0:
+    print("no simulation data in file: {}".format(fname))
+    exit(1)
+
+cutoff_results = [ result for result in results if result[0] > fidelity_cutoff ]
+cutoff_results.sort(key = lambda x: -x[0])
 
 with open(out_file,"w") as f:
     f.write("# samples: {}\n".format(samples))
+    f.write("# total results: {}\n".format(len(results)))
     f.write("# fidelity time seed target(s)\n")
-    for result in results:
+    for result in cutoff_results:
         f.write("{} {} {}".format(result[0],result[1],result[2]))
         for target in result[3]:
             f.write(" {}".format(target))
