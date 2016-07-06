4509789e0c6a8eaa5855fa035fb2672965d5e834
Michael A. Perlin <mika.perlin@gmail.com>
2016-07-06 16:54:33 +0200

    only print lattice if explicitly requested, and print coherence data to stdout

--- simulation.cpp
+++ simulation.cpp
@@ -20,6 +20,8 @@ using namespace Eigen;
 namespace fs = boost::filesystem;
 namespace po = boost::program_options;
 
+void line_break();
+
 int main(const int arg_num, const char *arg_vec[]) {
 
   // -------------------------------------------------------------------------------------
@@ -37,6 +39,7 @@ int main(const int arg_num, const char *arg_vec[]) {
      "seed for random number generator")
     ;
 
+  bool print_lattice;
   bool print_pairs;
   bool coherence_scan;
   bool rotation;
@@ -49,7 +52,11 @@ int main(const int arg_num, const char *arg_vec[]) {
 
   po::options_description simulations("Available simulations",help_text_length);
   simulations.add_options()
-    ("pairs", po::value<bool>(&print_pairs)->default_value(false)->implicit_value(true),
+    ("print_lattice",
+     po::value<bool>(&print_lattice)->default_value(false)->implicit_value(true),
+     "print C-13 lattice to a file")
+    ("print_pairs",
+     po::value<bool>(&print_pairs)->default_value(false)->implicit_value(true),
      "search for larmor pairs")
     ("scan", po::value<bool>(&coherence_scan)->default_value(false)->implicit_value(true),
      "perform coherence scan for effective larmor frequencies")
@@ -161,21 +168,12 @@ int main(const int arg_num, const char *arg_vec[]) {
      "time for each coherence measurement (microseconds)")
     ;
 
-  string lattice_file = "lattice-r[cell_radius]-s[seed].txt";
-  fs::path output_dir;
-  string output_suffix = "r[cell_radius]-s[seed]-c[cluster_size]-k[k_DD]-[ms].txt";
-  string output_suffix_with_input_lattice = "c[cluster_size]-k[k_DD]-[ms].txt";
-  bool no_output;
+  string lattice_file;
 
   po::options_description file_io("File IO",help_text_length);
   file_io.add_options()
     ("lattice_file", po::value<string>(&lattice_file),
      "input file defining system configuration")
-    ("output_dir", po::value<fs::path>(&output_dir)->default_value("./data"),
-     "directory for storing data")
-    ("output_suffix", po::value<string>(&output_suffix), "output file suffix")
-    ("no_output", po::value<bool>(&no_output)->default_value(false)->implicit_value(true),
-     "don't generate output files")
     ;
 
   po::options_description all("Allowed options");
@@ -199,13 +197,13 @@ int main(const int arg_num, const char *arg_vec[]) {
 
   // determine whether certain options were used
   bool using_input_lattice = inputs.count("lattice_file");
-  bool set_output_suffix = inputs.count("output_suffix");
   bool set_hyperfine_cutoff = !inputs["hyperfine_cutoff"].defaulted();
   bool set_c13_abundance = !inputs["c13_percentage"].defaulted();
   bool set_target_nuclei = inputs.count("target");
 
   // run a sanity check on inputs
-  if (!testing && (int(print_pairs)
+  if (!testing && (int(print_lattice)
+                   + int(print_pairs)
                    + int(coherence_scan)
                    + int(rotation)
                    + int(coupling)
@@ -218,12 +216,18 @@ int main(const int arg_num, const char *arg_vec[]) {
     return -1;
   }
 
+  // check lattice options
+  assert(!(print_lattice && using_input_lattice));
+  assert(!(using_input_lattice && !fs::exists(lattice_file)));
   assert(!(using_input_lattice && set_hyperfine_cutoff));
   assert(!(using_input_lattice && set_c13_abundance));
+
+  // check targeting options
   assert(!(set_target_nuclei && target_pairs));
+
+  // verify validity of other values
   assert(hyperfine_cutoff_in_kHz > 0);
   assert(c13_percentage >= 0 && c13_percentage <= 100);
-
   assert(max_cluster_size > 0);
   assert(ms == 1 || ms == -1);
   assert((k_DD_int == 1) || (k_DD_int == 3));
@@ -247,33 +251,8 @@ int main(const int arg_num, const char *arg_vec[]) {
   nv_polar = nv_polar_over_pi*pi;
   nv_azimuth = nv_azimuth_over_pi*pi;
 
-  // define path of lattice file defining system configuration
-  fs::path lattice_path;
-  if (using_input_lattice) {
-    if (!fs::exists(lattice_file)) {
-      cout << "File does not exist: " << lattice_file << endl;
-      return -1;
-    }
-    lattice_path = lattice_file;
-
-    if (!set_output_suffix) {
-      output_suffix =
-        lattice_path.stem().string() + "-" + output_suffix_with_input_lattice;
-    }
-
-  } else { // if !using_input_lattice
-    cell_radius = round(pow(abs(g_e*g_C13)/(4*pi*a0*a0*a0*hyperfine_cutoff),1.0/3));
-    cout << "Setting cell radius to: " << cell_radius << endl;
-
-    boost::replace_all(lattice_file, "[cell_radius]", to_string(cell_radius));
-    boost::replace_all(lattice_file, "[seed]", to_string(seed));
-    lattice_path = output_dir/fs::path(lattice_file);
-  }
-
   uniform_real_distribution<double> rnd(0.0,1.0); // uniform distribution on [0,1)
   mt19937_64 generator(seed); // use and seed the 64-bit Mersenne Twister 19937 generator
-
-  fs::create_directory(output_dir); // create data directory
 
   // -------------------------------------------------------------------------------------
   // Construct lattice of nuclei
@@ -303,22 +282,19 @@ int main(const int arg_num, const char *arg_vec[]) {
     cout << "Placed " << nuclei.size() << " C-13 nuclei\n\n";
     if (nuclei.size() == 0) return 0;
 
-    // write cell radius and nucleus positions to file
-    if (!no_output && !print_pairs) {
-      ofstream lattice(lattice_path.string());
-      lattice << "# cell radius: " << cell_radius << endl;
+    if (print_lattice) {
       for (uint i = 0; i < nuclei.size(); i++) {
-        lattice << nuclei.at(i)(0) << ' '
-                << nuclei.at(i)(1) << ' '
-                << nuclei.at(i)(2) << endl;
+        cout << nuclei.at(i)(0) << " "
+             << nuclei.at(i)(1) << " "
+             << nuclei.at(i)(2) << endl;
       }
-      lattice.close();
+      return 0;
     }
 
   } else { // if using_input_lattice, read in the lattice
 
     string line;
-    ifstream lattice(lattice_path.string());
+    ifstream lattice(lattice_file);
 
     // get cell_radius
     getline(lattice,line,' ');
@@ -469,13 +445,6 @@ int main(const int arg_num, const char *arg_vec[]) {
   }
   cout << endl;
 
-  // now that we are done with initialization, fix up the output filename suffix
-  boost::replace_all(output_suffix, "[cell_radius]", to_string(cell_radius));
-  boost::replace_all(output_suffix, "[seed]", to_string(seed));
-  boost::replace_all(output_suffix, "[cluster_size]", to_string(max_cluster_size));
-  boost::replace_all(output_suffix, "[k_DD]", to_string(k_DD_int));
-  boost::replace_all(output_suffix, "[ms]", (ms > 0)?"up":"dn");
-
   // initialize nv_system object
   const nv_system nv(nuclei, clusters, ms, g_C13*static_Bz_in_gauss*gauss, k_DD,
                      scale_factor, integration_factor, no_nn);
@@ -498,19 +467,16 @@ int main(const int arg_num, const char *arg_vec[]) {
       if (w_larmor.at(i) > w_max) w_max = w_larmor.at(i);
     }
 
-    // print effective larmor frequencies and NV couping strengths to output file
-    if (!no_output) {
-      fs::path larmor_path = output_dir/fs::path("larmor-"+output_suffix);
-      ofstream larmor_file(larmor_path.string());
-      larmor_file << "# w_larmor A_perp\n";
-      for (uint i = 0; i < nv.nuclei.size(); i++) {
-        larmor_file << w_larmor.at(i) << " " << A_perp.at(i) << endl;
-      }
-      larmor_file.close();
+    // print effective larmor frequencies and NV couping strengths
+    line_break();
+    cout << "# w_larmor A_perp\n";
+    for (uint i = 0; i < nv.nuclei.size(); i++) {
+      cout << w_larmor.at(i) << " " << A_perp.at(i) << endl;
     }
 
     // perform coherence scan
-    cout << "Beginning coherence scan\n";
+    line_break();
+    cout << "# w_scan coherence\n";
     vector<double> w_scan(scan_bins);
     vector<double> coherence(scan_bins);
 
@@ -520,24 +486,9 @@ int main(const int arg_num, const char *arg_vec[]) {
     for (uint i = 0; i < scan_bins; i++) {
       w_scan.at(i) = w_start + i*(w_end-w_start)/scan_bins;
       coherence.at(i) = coherence_measurement(nv, w_scan.at(i), f_DD, scan_time);
-      cout << "(" << i+1 << "/" << scan_bins << ") "
-           << w_scan.at(i)/(2*pi*1e3) << " " << coherence.at(i) << endl;
+      cout << w_scan.at(i) << " " << coherence.at(i) << endl;
     }
 
-    // print coherence scan results to output file
-    if (!no_output) {
-      fs::path scan_path = output_dir/fs::path("scan-"+output_suffix);
-      ofstream scan_file(scan_path.string());
-      scan_file << "# cluster coupling factor (Hz): " << cluster_coupling << endl;
-      scan_file << "# f_DD: " << f_DD << endl;
-      scan_file << "# scan time: " << scan_time << endl;
-      scan_file << endl;
-      scan_file << "# w_scan coherence\n";
-      for (uint i = 0; i < scan_bins; i++) {
-        scan_file << w_scan.at(i) << " " << coherence.at(i) << endl;
-      }
-      scan_file.close();
-    }
   }
 
   // -------------------------------------------------------------------------------------
@@ -695,4 +646,9 @@ int main(const int arg_num, const char *arg_vec[]) {
 
   }
 
+}
+
+void line_break() {
+  for (uint i = 0; i < 90; i++) cout << "-";
+  cout << endl;
 }
