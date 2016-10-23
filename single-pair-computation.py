6380b8e6b996d7f542cd4d8e4ec4fadbc32545bf
Michael A. Perlin <mika.perlin@gmail.com>
2016-10-22 19:31:04 -0600

    improvements to single-pair-computation.py

--- single-pair-computation.py
+++ single-pair-computation.py
@@ -31,48 +31,26 @@ a0 = 0.35668 * nm # diamond lattice parameter (unit cell side length) at 300 K
 zhat = np.array([1,1,1])/np.sqrt(3) # static magnetic field direction; NV axis
 
 # diamond lattice vectors
-ao = np.array([1,1,1])/2.
-a1 = np.array([0,1,1])
-a2 = np.array([1,0,1])
-a3 = np.array([1,1,0])
+ao = np.array([1,1,1])/4.
+a1 = np.array([0,1,1])/2.
+a2 = np.array([1,0,1])/2.
+a3 = np.array([1,1,0])/2.
+
 
 norm = np.linalg.norm
+def r_vec(iv): return iv[0]*ao + iv[1]*a1 + iv[2]*a2 + iv[3]*a3
 def hat(v): return v/norm(v)
-def A(b,l,m,n):
-    r = b*ao + l*a1 + m*a2 + n*a3
-    return norm( ge*gC13/(4*np.pi*(norm(r)*a0/2)**3) *
+
+def A(iv):
+    r = r_vec(iv)
+    return norm( ge*gC13/(4*np.pi*(norm(r)*a0)**3) *
                  (zhat-3*np.dot(hat(r),zhat)*hat(r)) )
 
-def z_projection_sum(iv): return abs(3*iv[0] + 4*(iv[1]+iv[2]+iv[3]))
-def xy_projection_sum(iv): return 3*(iv[1]**2+iv[2]**2+iv[3]**2) - (iv[1]+iv[2]+iv[3])**2
+def z_projection_sum(iv):
+    return abs(3*iv[0] + 4*(iv[1]+iv[2]+iv[3]))
+def xy_projection_sum(iv):
+    return 3*(iv[1]**2+iv[2]**2+iv[3]**2) - (iv[1]+iv[2]+iv[3])**2
 
-# maximum allowable magnitude of l, m, or n
-max_lmn = int((2*abs(ge*gC13) / (np.pi * a0**3 * hyperfine_cutoff))**(1./3)+1./2)
-
-# list of all larmor sets
-larmor_sets = []
-
-# loop over each integer z_sum up to the maximum possible value given the hyperfine cutoff
-# start at 1 to exclude nuclei which lie in the x-y plane
-for z_sum in range(1,12*max_lmn+1):
-
-    # vector of integers (b,l,m,n) with this z_sum
-    z_sum_solutions = set([ (b,l,m,n)
-                            for b in [0,1]
-                            for l in range(-max_lmn,max_lmn+1)
-                            for m in range(-max_lmn,max_lmn+1)
-                            for n in range(-max_lmn,max_lmn+1)
-                            if z_projection_sum((b,l,m,n)) == z_sum
-                            if A(b,l,m,n) > hyperfine_cutoff ])
-
-    # determine all xy_sums for these solutions of z_sum
-    xy_sums = set([ xy_projection_sum(z_solution) for z_solution in z_sum_solutions ])
-
-    # collect solutions of z_sum into larmor sets if they have the same xy_sum
-    for xy_sum in xy_sums:
-        larmor_set = [ z_solution for z_solution in z_sum_solutions
-                       if xy_projection_sum(z_solution) == xy_sum ]
-        larmor_sets.append(larmor_set)
 
 # return a the sizes of the nontrivial parallel subsets in a given equivalence class
 def parallel_subset_sizes(larmor_set):
@@ -96,8 +74,34 @@ def parallel_subset_sizes(larmor_set):
     parallel_set_sizes = [ len(parallel_set) for parallel_set in parallel_sets ]
     return [ size for size in parallel_set_sizes if size > 1 ]
 
-larmor_set_info = [ (len(larmor_set), parallel_subset_sizes(larmor_set))
-                    for larmor_set in larmor_sets ]
+
+# maximum allowable magnitude of l, m, or n
+max_lmn = int((2*abs(ge*gC13) / (np.pi * a0**3 * hyperfine_cutoff))**(1./3)+1./2)
+
+# list of all larmor sets
+larmor_sets = []
+
+# loop over each integer z_sum up to the maximum possible value given the hyperfine cutoff
+# start at 1 to exclude nuclei which lie in the x-y plane
+for z_sum in range(1,12*max_lmn+1):
+
+    # vector of integers (b,l,m,n) with this z_sum
+    z_sum_solutions = set([ (b,l,m,n)
+                            for b in [0,1]
+                            for l in range(-max_lmn,max_lmn+1)
+                            for m in range(-max_lmn,max_lmn+1)
+                            for n in range(-max_lmn,max_lmn+1)
+                            if z_projection_sum((b,l,m,n)) == z_sum
+                            if A((b,l,m,n)) > hyperfine_cutoff ])
+
+    # determine all xy_sums for these solutions of z_sum
+    xy_sums = set([ xy_projection_sum(z_solution) for z_solution in z_sum_solutions ])
+
+    # collect solutions of z_sum into larmor sets if they have the same xy_sum
+    for xy_sum in xy_sums:
+        larmor_set = [ z_solution for z_solution in z_sum_solutions
+                       if xy_projection_sum(z_solution) == xy_sum ]
+        larmor_sets.append(larmor_set)
 
 # return product of elements in a list
 def product(list):
@@ -111,7 +115,7 @@ def nCk(n,k): return product([ n+1-i for i in range(1,k+1)]) / product(range(1,k
 # probability of having at least one larmor set with exactly two addressable nuclei
 probability = 1 - product([ 1 - c13_abundance**2 * (1-c13_abundance)**(R-2) *
                             ( nCk(R,2) - sum([ nCk(s,2) for s in pss ]) )
-                            for eci in larmor_set_info
-                            for R in [eci[0]]
-                            for pss in [eci[1]] ])
+                            for larmor_set in larmor_sets
+                            for R in [len(larmor_set)]
+                            for pss in [parallel_subset_sizes(larmor_set)] ])
 print(probability)
